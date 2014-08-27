f8b32d3ac484bccc4b38ded49c7a88b171ab27a0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-08-28 08:49:17 +0900

    move google map operation to GoogleMapOperator class

--- app/src/main/java/xyz/yyagi/onetouchsearch/MainActivity.java
+++ app/src/main/java/xyz/yyagi/onetouchsearch/MainActivity.java
@@ -3,13 +3,11 @@ package xyz.yyagi.onetouchsearch;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
-import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -27,35 +25,27 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import xyz.yyagi.onetouchsearch.api.GoogleMapApiClient;
+import xyz.yyagi.onetouchsearch.api.GoogleMapOperator;
 
 public class MainActivity extends FragmentActivity
         implements LocationListener , Response.Listener<JSONObject>, Response.ErrorListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     public LocationManager mLocationManager;
-    private boolean mDisplayedMarker = false;
-    private Marker mCurrentPosMarker = null;
+    private boolean mDisplayedMarker = false;;
     private String mGooglePlaceAPIKey;
     private Position mCurrentPosition;
     private View mProgressView;
     private View mMapFragment;
+    private GoogleMapOperator mMapOperator;
 
-    private static final int ZOOM = 15;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -70,7 +60,6 @@ public class MainActivity extends FragmentActivity
         showProgress(true);
         setLocationProvider();
         setUpMapIfNeeded();
-        setCurrentPosMarkerToMap(mCurrentPosition.getLat(), mCurrentPosition.getLng());
     }
 
     @Override
@@ -109,27 +98,13 @@ public class MainActivity extends FragmentActivity
     }
 
     private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+        if (mMapOperator == null) {
+            GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
-            moveCamera();
+            mMapOperator = new GoogleMapOperator(map);
+            mMapOperator.moveCamera(mCurrentPosition);
+            mMapOperator.setCurrentPosMarkerToMap(mCurrentPosition);
         }
-    }
-
-    private void addMarkerToMap(String title, Double lat, Double lng) {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(title));
-    }
-
-    private void setCurrentPosMarkerToMap(Double lat, Double lng) {
-        if (mCurrentPosMarker != null) {
-            mCurrentPosMarker.remove();
-        }
-
-        mCurrentPosMarker = mMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(lat, lng))
-                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.current)));
     }
 
     private void setLocationProvider() {
@@ -152,10 +127,10 @@ public class MainActivity extends FragmentActivity
 
         if (!mDisplayedMarker) {
             fetchPlaces();
-            moveCamera();
+            mMapOperator.moveCamera(mCurrentPosition);
             mDisplayedMarker = true;
         }
-        setCurrentPosMarkerToMap(mCurrentPosition.getLat(), mCurrentPosition.getLng());
+        mMapOperator.setCurrentPosMarkerToMap(mCurrentPosition);
     }
 
     @Override
@@ -168,12 +143,6 @@ public class MainActivity extends FragmentActivity
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
-
-    private void moveCamera() {
-        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(
-                new LatLng(mCurrentPosition.getLat(), mCurrentPosition.getLng()), ZOOM);
-        mMap.moveCamera(cu);
     }
 
     private void fetchPlaces() {
@@ -201,7 +170,7 @@ public class MainActivity extends FragmentActivity
                 String name = results.getJSONObject(i).getString("name");
                 Double lat = results.getJSONObject(i).getJSONObject("geometry").getJSONObject("location").getDouble("lat");
                 Double lng = results.getJSONObject(i).getJSONObject("geometry").getJSONObject("location").getDouble("lng");
-                addMarkerToMap(name, lat, lng);
+                mMapOperator.addMarkerToMap(name, lat, lng);
             }
 
         } catch (JSONException e ) {
---
+++ app/src/main/java/xyz/yyagi/onetouchsearch/api/GoogleMapOperator.java
@@ -0,0 +1,46 @@
+package xyz.yyagi.onetouchsearch.api;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import xyz.yyagi.onetouchsearch.Position;
+import xyz.yyagi.onetouchsearch.R;
+
+/**
+ * Created by yaginuma on 14/08/28.
+ */
+public class GoogleMapOperator {
+    private GoogleMap mMap;
+    private Marker mCurrentPosMarker;
+    private static final int ZOOM = 15;
+
+    public GoogleMapOperator(GoogleMap mMap) {
+        this.mMap = mMap;
+    }
+
+    public void addMarkerToMap(String title, Double lat, Double lng) {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(title));
+    }
+
+    public void setCurrentPosMarkerToMap(Position pos) {
+        if (mCurrentPosMarker != null) {
+            mCurrentPosMarker.remove();
+        }
+
+        mCurrentPosMarker = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(pos.getLat(), pos.getLng()))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.current)));
+    }
+
+    public void moveCamera(Position pos) {
+        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(
+                new LatLng(pos.getLat(), pos.getLng()), ZOOM);
+        mMap.moveCamera(cu);
+    }
+
+}
